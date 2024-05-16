ae3aaa30494e218902877e2368907d7b2b2c5bf3
Todd-3 <roman.r.todd@gmail.com>
2024-05-16 08:36:35 -0700

    Updating Comment

--- markdown_convert.py
+++ markdown_convert.py
@@ -12,7 +12,7 @@ link_replace_regex = re.compile(r'\[([^\[\]]+)]\(([^()]+)\)')
 link_replace_func = lambda parts: "<a href=\"{1}\">{0}</a>".format(*parts.groups())
 
 # bold replacement
-# **text** -> <b>text</b>
+# **text** | __text__ -> <strong>text</strong>
 bold_replace_regex = re.compile(r'[*_]{2}([^*]+)[*_]{2}')
 bold_replace_func = lambda parts: "<strong>{0}</strong>".format(*parts.groups())
 
